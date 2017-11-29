a6f1e776d0ab80438e5f6f55e1141a6253b62092
Bobby Ton <bobbyt1997@gmail.com>
2017-11-28 20:01:50 -0600

    model.js is used to depict what each user object will look like.

--- README.md
+++ README.md
@@ -7,11 +7,15 @@ Create/View/Delete a user:
 
 Create/View/Delete a routine:
 	POST /users/:userId/routines
-	POST /users/:userId/routines/:routineId/
+	POST /users/:userId/routines/:routineId/workouts
 	GET /users/:userId/routines
 	GET /users/:userId/routines/:routineId
+	GET /users/:userId/routines/:routineId/workouts
+	GET /users/:userId/routines/:routineId/workouts/:workoutId
 	DELETE /users/:userId/routines/:routineId
 	DELETE /users/:userId/routines
+	DELETE /users/:userId/routines/:routineId/workouts
+	DELETE /users/:userId/routines/:routineId/workouts/:workoutId
 
 Create/View/Delete/Update a statistic(stat):
 	POST /users/:userId/stats
---
+++ model.js
@@ -0,0 +1,47 @@
+var users = {
+	1:{
+		"name":"Ken Ellorando",
+		"weight": 185,
+		"height": 72,
+		"sex": "male",
+		"stats":{
+			"ohp": 185,
+			"bench": 225,
+			"deadlift":	275	
+		},
+		"workout": {
+			1: {
+				"name": "AWESOME CHEST DAY",
+				"bench": {
+					"reps": 10,
+					"sets": 5
+				},
+				"flys": {
+					"reps": 1,
+					"sets": 22
+				}
+			},
+			2: {
+				"name": "AWESOME LEG DAY",
+				"squat": {
+					"reps": 10,
+					"sets": 5
+				},
+				"flys": {
+					"reps": 1,
+					"sets": 22
+				}
+			}
+		}
+		"weight":{
+			{
+				"date": null,
+				"weight": 185
+			}
+		},
+		"diet": {
+			"goal": null,
+			"actual": null
+		}
+	}
+}
