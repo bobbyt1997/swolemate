224a1d06b99b571a5eea74a32bcf99783551fcd7
Bobby Ton <bobbyt1997@gmail.com>
2017-11-30 15:10:53 -0600

    Added server

--- server/server.js
+++ server/server.js
@@ -4,12 +4,24 @@ var fs = require("fs");
 var parser = require("body-parser");
 var path = require('path');
 
-var idCounter = 0;
-var reminderIDCounter = 0;
-var users = [];
+var userIdCtr = 0;
+var users = {};
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(parser.json());
 app.use(parser.urlencoded({ 
    extended: true 
 }));
+
+
+
+
+//-----------------------------------------------------------------------
+var server = app.listen(8080, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("REMINDER APP listening at http://%s:%s", host, port);
+
+});
